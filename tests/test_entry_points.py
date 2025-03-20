532148a284e14191c76602c75c33df0d34b3cdd3
Jean Henry <126060567+ansjhenry@users.noreply.github.com>
2025-03-20 17:34:47 +0100

    feat: Add register and unregister entry points for Extensions Manager (#28)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
    (cherry picked from commit 94e5e1561b41f3d074cd089a549ebc997678e4d3)

---
+++ tests/test_entry_points.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2023 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from pathlib import Path
+
+import ansys.scade.python_wrapper as pw
+
+
+def test_srg():
+    path = pw.srg()
+    assert path
+    assert Path(path).exists()
+
+
+def test_get_srg_name():
+    name = pw.get_srg_name()
+    assert name
+    assert name.startswith('python_wrapper2')
+    assert name.endswith('.srg')
